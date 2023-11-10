3603d138f94491c15ce158251b848101444f21f4
Nikita <nikita.shalashkov@gmail.com>
2023-11-10 20:06:31 +0300

    Add content message for openai model

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,43 @@ const openai = new OpenAI({
 
 const instructionMessage = {
   role: "system",
-  content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
+  content: "OpenAI Code Generation Instructions \
+  \
+  ## Task Description \
+  You are a code generator. Your task is to analyze and process a given dataset of financial transactions. \
+  \
+  ## Dataset Format \
+  The dataset consists of financial transactions with the following format: \
+  - Date: The date of the transaction in the format YYYY-MM-DD. \
+  - Type: The type of transaction (e.g., Shopping, Food, Freelance Work). \
+  - Company: The company involved in the transaction.\
+  - Amount: The amount of money involved, prefixed with "+" for income and "-" for expenses. \
+  \
+  ## Output Format \
+  Generate Markdown code snippets to perform the following tasks: \
+  1. Calculate the total income and total expenses. \
+  2. List transactions for a specific type. \
+  3. Identify transactions with a specific company. \
+  4. Determine the balance (total income - total expenses). \
+  \
+  ## Code Comments \
+  Use code comments to explain your code snippets clearly. \
+  \
+  ## Example Transaction: \
+  Date: 2023-01-01, Type: Shopping, Company: Amazon, Amount: +106.50 \
+  \
+  ## Example Code Snippet: \
+  // Task 1: Calculate total income and total expenses \
+  // Your code here... \
+  \
+  // Task 2: List transactions for a specific type \
+  // Your code here... \
+  \
+  // Task 3: Identify transactions with a specific company \
+  // Your code here... \
+  \
+  // Task 4: Determine the balance \
+  // Your code here..."
 };
 
 

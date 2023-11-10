cc025bf1768ac282b2d318c31ec61f0d8a330734
Nikita <nikita.shalashkov@gmail.com>
2023-11-10 23:46:54 +0300

    Prompt for OpenAI API almost done

--- backend/__main__.py
+++ backend/__main__.py
@@ -4,6 +4,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from openai import OpenAI
 
+from utils import sequence_prediction
+
 app = Flask(__name__)
 CORS(app)
 
@@ -14,111 +16,105 @@ load_dotenv()
 # initialize OpenAI API and instruction message
 openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+# ! TODO: add to dataset date and priority columns! 
 instruction_message = {
     "role": "system",
-    "content": """You are a finance assistant in banking.. Your task is to analyze and process a given dataset of financial transactions.
-    ## Dataset Format 
-    The dataset consists of financial transactions with the following format: 
-    - Date: The date of the transaction in the format YYYY-MM-DD. 
-    - Type: The type of transaction (e.g., Shopping, Food, Freelance Work). 
-    - Company: The company involved in the transaction.
-    - Amount: The amount of money involved, prefixed with "+" for income and "-" for expenses. 
-    ## Output Format 
-    Generate Markdown Answer snippets to perform the following tasks: 
-    1. Calculate the total income and total expenses. 
-    2. List transactions for a specific type. 
-    3. Identify transactions with a specific company. 
-    4. Determine the balance (total income - total expenses). 
-    ## Answer Comments 
-    Use answer comments to explain your Answer snippets clearly. 
-    ## Example Transaction: 
-    Date: 2023-01-01, Type: Shopping, Company: Amazon, Amount: +106.50
-    ## Example Answer Snippet: 
-    // Task 1: Calculate total income and total expenses 
-    // Your answer here... 
-    // Task 2: List transactions for a specific type 
-    // Your answer here... 
-    // Task 3: Identify transactions with a specific company
-    // Your answer here... 
-    // Task 4: Determine the balance 
-    // Your answer here..."""
+    "content": """
+    You are a Finance Assistant who is giving advice through client transaction history. 
+    Your role is to help to navigate and manage client finances with ease. 
+    Dataset Format is next:
+    1. Type - the category of the transaction (e.g., Shopping, Food, Freelance Work);
+    2. Amount - the monetary value, with "+" indicating income and "-" indicating expenses.
+    Use client transaction history in your response to indicate to a field, where he can save money (and also tell him an amount that he can save).
+    All transaction are going to be in the next message.
+    """
 }
 
 
 @app.get("/assistant")
 def assistant():
     # values = request.json
-    message = {
-        "role": "user",
-        "content": """Date: 2023-09-01
-            Type: Food
-            Company: Green Valley Groceries
-            Amount: -$65.50
-            Date: 2023-09-05
-            Type: Entertainment
-            Company: Cinemax Theaters
-            Amount: -$20.00
-            Date: 2023-09-10
-            Type: Utilities
-            Company: Bright Energy
-            Amount: -$110.75
-            Date: 2023-09-15
-            Type: Subscription
-            Company: NetStream Services
-            Amount: -$12.99
-            Date: 2023-09-18
-            Type: Food
-            Company: Bella's Pizzeria
-            Amount: -$30.25
-            Date: 2023-09-20
-            Type: Income
-            Company: Luxonis
-            Amount: +$2500.00
-            Date: 2023-09-23
-            Type: Clothing
-            Company: Trendy Fashion Outlet
-            Amount: -$85.00
-            Date: 2023-09-27
-            Type: Electronics
-            Company: TechWorld Gadgets
-            Amount: -$220.00
-            Date: 2023-10-01
-            Type: Food
-            Company: Green Valley Groceries
-            Amount: -$72.00
-            Date: 2023-10-05
-            Type: Entertainment
-            Company: PlayZone Arcade
-            Amount: -$15.50
-            Date: 2023-10-08
-            Type: Transportation
-            Company: City Cab Services
-            Amount: -$40.00
-            Date: 2023-10-15
-            Type: Subscription
-            Company: MusicBeat Premium
-            Amount: -$9.99
-            Date: 2023-10-18
-            Type: Food
-            Company: Fresh Bistro
-            Amount: -$47.35
-            Date: 2023-10-20
-            Type: Income
-            Company: Luxonis
-            Amount: +$2500.00
-            Date: 2023-10-25
-            Type: Medical
-            Company: City Health Clinic
-            Amount: -$120.00""",
-    }
+    messages = [
+        {
+            "role": "user",
+            "content": """
+            Client transaction history (json format):
+            [
+                {
+                    Type: Food,
+                    Amount: -$65.50
+                },
+                {
+                    Type: Entertainment,
+                    Amount: -$20.00
+                },
+                {
+                    Type: Utilities,
+                    Amount: -$110.75
+                },
+                {
+                    Type: Subscription,
+                    Amount: -$12.99
+                },
+                {
+                    Type: Food,
+                    Amount: -$30.25
+                },
+                {
+                    Type: Income,
+                    Amount: +$2500.00
+                },
+                {
+                    Type: Clothing,
+                    Amount: -$85.00
+                },
+                {
+                    Type: Electronics,
+                    Amount: -$220.00
+                },
+                {
+                    Type: Food,
+                    Amount: -$72.00
+                },
+                {
+                    Type: Entertainment,
+                    Amount: -$15.50
+                },
+                {
+                    Type: Transportation,
+                    Amount: -$40.00
+                },
+                {
+                    Type: Subscription,
+                    Amount: -$9.99
+                },
+                {
+                    Type: Food,
+                    Amount: -$47.35
+                },
+                {
+                    Type: Income,
+                    Amount: +$2500.00
+                },
+                {
+                    Type: Medical,
+                    Amount: -$120.00
+                }
+            ]
+            # Client question: How can I safe more money?"""
+        }
+    ]
 
     completion = openai.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[instruction_message, message],
+        messages=[instruction_message, *messages],
     )
     print(completion.choices[0].message.content)
     return completion.choices[0].message.content
 
+@app.get("/forecast")
+def forecast():
+    return jsonify(sequence_prediction())
 
 if __name__ == "__main__":
     app.run(port=os.getenv("FLASK_PORT", 5000), debug=True)
---
+++ backend/utils/forecast.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from prophet import Prophet
+import numpy as np
+
+
+# Generate more random data for the example
+# Let's create a dataset with a slightly more complex pattern
+def sequence_prediction():
+  np.random.seed(42)  # for reproducibility
+
+  # Generate random data with a trend and some seasonality
+  days = pd.date_range(start='2020-01-01', periods=2000, freq='D')
+  data = np.random.normal(loc=0, scale=5, size=2000).cumsum()
+  seasonality = np.sin(np.linspace(0, 20, 2000)) * 20
+  noise = np.random.normal(loc=0, scale=2, size=2000)
+
+  # Combine trend, seasonality, and noise
+  y = 50 + data + seasonality + noise
+
+  # Create the DataFrame
+  df = pd.DataFrame({'ds': days, 'y': y})
+
+  print(df.tail())
+
+  # Initialize a Prophet model
+  model = Prophet(interval_width=0.92, daily_seasonality=True)
+
+  # Fit the model on the dataset
+  model.fit(df)
+
+  # Create a DataFrame for future dates to predict
+  periods = 60
+  future = model.make_future_dataframe(periods=periods)
+
+  # Predict values for future dates
+  forecast = model.predict(future)
+
+  # Display the forecast
+  return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_json()

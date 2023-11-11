7d23474d3a27266111767077f3622f619b4910c9
Mark Minerov <mark@metamorf.ai>
2023-11-11 01:01:35 +0100

    fixe

--- front/src/main.ts
+++ front/src/main.ts
@@ -8,6 +8,10 @@ import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Menu from "primevue/menu";
 import InputText from "primevue/inputtext";
+import Message from "primevue/message";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import VueWriter from "vue-writer";
 
 import "./style.scss";
 
@@ -16,6 +20,7 @@ import "primeicons/primeicons.css";
 
 const app = createApp(App);
 app.use(PrimeVue, { ripple: true });
+app.use(VueAxios, axios);
 app.component("Button", Button);
 app.component("Menubar", Menubar);
 app.component("Avatar", Avatar);
@@ -23,4 +28,6 @@ app.component("Menu", Menu);
 app.component("DataTable", DataTable);
 app.component("Column", Column);
 app.component("InputText", InputText);
+app.component("Message", Message);
+app.use(VueWriter);
 app.mount("#app");

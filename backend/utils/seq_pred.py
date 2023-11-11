7b588c9decfbeb32ec35603a9de53ad0c5412685
Michael Ruzmetov <mimisha2014@gmail.com>
2023-11-11 04:36:54 +0100

    fix index problem (forecast)

--- backend/utils/seq_pred.py
+++ backend/utils/seq_pred.py
@@ -12,7 +12,7 @@ current_balance = 1000
 data = [
     ["2023-01-01", "Groceries", -45],
     ["2023-01-01", "Utilities", -30],
-    ["2023-01-02", "Rent", -500],
+    ["2023-01-02", "Rent", -250],
     ["2023-01-02", "Freelance Income", 200],
     ["2023-01-03", "Transport", -20],
     ["2023-01-04", "Dining Out", -35],
@@ -25,7 +25,7 @@ data = [
     ["2023-01-10", "Utilities", -25],
     ["2023-01-11", "Clothing", -75],
     ["2023-01-12", "Freelance Income", 150],
-    ["2023-01-13", "Rent", -500],
+    ["2023-01-13", "Rent", -250],
     ["2023-01-14", "Groceries", -55],
     ["2023-01-15", "Dining Out", -30],
     ["2023-01-16", "Gym Membership", -25],
@@ -36,7 +36,7 @@ data = [
     ["2023-01-22", "Utilities", -30],
     ["2023-01-23", "Clothing", -80],
     ["2023-01-24", "Freelance Income", 250],
-    ["2023-01-25", "Rent", -500],
+    ["2023-01-25", "Rent", -250],
     ["2023-01-26", "Groceries", -65],
     ["2023-01-27", "Dining Out", -40],
     ["2023-01-28", "Gym Membership", -25],
@@ -47,7 +47,7 @@ data = [
     ["2023-02-02", "Utilities", -35],
     ["2023-02-03", "Clothing", -90],
     ["2023-02-04", "Freelance Income", 300],
-    ["2023-02-05", "Rent", -550],
+    ["2023-02-05", "Rent", -275],
     ["2023-02-06", "Groceries", -50],
     ["2023-02-07", "Dining Out", -45],
     ["2023-02-08", "Gym Membership", -30],
@@ -78,14 +78,11 @@ model = Prophet(interval_width=0.92, daily_seasonality=True)
 model.fit(df)
 
 # Create a DataFrame for future dates to predict
-periods = 15
+periods = 10
 future = model.make_future_dataframe(periods=periods, freq='D')
 
 # Predict values for future dates
 forecast = model.predict(future)
-
-# Concatenate the original dataset and the forecasted values
-# df_forecast = pd.concat([df, forecast], ignore_index=True)
 
 # Plot the historical data
 plt.plot(df['ds'], df['y'], label='Historical Data', marker='.', linestyle='-')

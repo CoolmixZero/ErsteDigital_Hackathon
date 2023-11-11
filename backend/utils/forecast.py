be3c3a02b2693a76d71b6858031514abf7cc5e13
Michael Ruzmetov <mimisha2014@gmail.com>
2023-11-11 05:14:07 +0100

    idx fix

--- backend/utils/forecast.py
+++ backend/utils/forecast.py
@@ -83,8 +83,8 @@ def sequence_prediction():
 
   forecast['ds'] = forecast['ds'].dt.strftime('%Y-%m-%d')
   forecast['y'] = df['y']
-  forecast['yhat'] = forecast['yhat'].iloc[len(df):].astype(int)
-  forecast['y'][len(df)-1] = forecast['yhat'].iloc[len(df)]
+  forecast['yhat'] = forecast['yhat'].iloc[len(df)-1:].astype(int)
+  forecast['y'][len(df)-1] = forecast['yhat'].iloc[len(df)-1]
   
   final_forecast = forecast[['ds', 'y', 'yhat']].to_json()
   return final_forecast
